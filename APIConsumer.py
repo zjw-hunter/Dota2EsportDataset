d27c26852adbf4939cbbccea43d2e860611ec7c1
zjw.hunter <zjw.hunter@gmail.com>
2021-09-02 19:19:16 -0400

    made dbc and improoved api consumer

--- APIConsumer.py
+++ APIConsumer.py
@@ -1,18 +1,24 @@
 import requests
-
-
-
+import time
 
 class APIConsumer():
 
-    LiquipediaBaseURL = "https://liquipedia.net/dota2/api.php?action=askargs&format=json&conditions=Category%3ATier%201%20Tournaments&utf8=1"
+    rateLimit = 1250
 
+    LiquipediaGetT1s = "https://liquipedia.net/dota2/api.php?action=askargs&format=json&conditions=Category%3ATier%201%20Tournaments&utf8=1"
+    LiquipediaGetQs = "https://liquipedia.net/dota2/api.php?action=askargs&format=json&conditions=Category%3AQualifier%20Tournaments&utf8=1"
+    LiquipediaGetArticleByTitle = "https://liquipedia.net/dota2/api.php?action=query&format=json&prop=cirrusdoc&list=&titles="
+    LiquipediaSpace = "%20"
+    LiquipediaSuffix = "&utf8=1"
 
     def __init__(self):
         pass
 
-    def getTournaments(self):
+    def getRequest(self, url):
         try:
-            response = requests.get(self.LiquipediaBaseURL)
-
-        except Exception as e: print(e)
+            time.sleep(self.rateLimit)
+            response = requests.get(url)
+            return(response)
+        except Exception as e:
+            print(e)
+            return False
---
+++ DatabaseConnector.py
@@ -0,0 +1,41 @@
+from pymongo import MongoClient
+from pprint import pprint
+
+from pymongo.operations import InsertOne
+
+class DatabaseConnector():
+
+    DOTA_COLLECTIONS = ["Players", "Matches", "Teams", "Leagues", "Heroes"]
+    client = MongoClient("mongodb://localhost:27017")
+    db = client.Dota2ProMatches
+    def __init__(self) -> None:
+        pass
+
+    def getServerStatus(self):
+        return(self.db.command("serverStatus"))
+
+    def getCollections(self):
+        return(self.DOTA_COLLECTIONS)
+
+    # obj is the data, cName is the collection to add the document to.
+    # true if operation worked, otherwise false
+    def insertMany(self, data, cName):
+        if(cName not in self.DOTA_COLLECTIONS):
+            return False
+        if(data is None):
+            return False
+        try:
+            if( cName == "Players"):
+                self.db.Players.insert_many(data)
+            elif( cName == "Matches"):
+                self.db.Matches.insert_many(data)
+            elif( cName == "Teams"):
+                self.db.Teams.insert_many(data)
+            elif( cName == "Leagues"):
+                self.db.Leagues.insert_many(data)
+            else:
+                return False
+            return True
+        except Exception as e:
+            print(e)
+            return False
